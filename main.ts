690303285f22fa74c41202fe977290e0320f74e0
rogerrb <roger_R_berg@hotmail.com>
2021-10-25 18:40:34 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,30 +3,54 @@ scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.floorLight0, function (sp
     music.pewPew.play()
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (true) {
+    if (controller.dx() != 0 || controller.dy() != 0) {
         projectile = sprites.createProjectileFromSprite(img`
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
-            . . . . . 4 4 4 4 4 . . . . . . 
-            . . . 4 4 4 5 5 5 d 4 4 4 4 . . 
-            . . 4 d 5 d 5 5 5 d d d 4 4 . . 
-            . . 4 5 5 1 1 1 d d 5 5 5 4 . . 
-            . 4 5 5 5 1 1 1 5 1 1 5 5 4 4 . 
-            . 4 d d 1 1 5 5 5 1 1 5 5 d 4 . 
-            . 4 5 5 1 1 5 1 1 5 5 d d d 4 . 
-            . 2 5 5 5 d 1 1 1 5 1 1 5 5 2 . 
-            . 2 d 5 5 d 1 1 1 5 1 1 5 5 2 . 
-            . . 2 4 d d 5 5 5 5 d d 5 4 . . 
-            . . . 2 2 4 d 5 5 d d 4 4 . . . 
-            . . 2 2 2 2 2 4 4 4 2 2 2 . . . 
-            . . . 2 2 4 4 4 4 4 4 2 2 . . . 
-            . . . . . 2 2 2 2 2 2 . . . . . 
-            `, mySprite, controller.dx(100), controller.dy(100))
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 4 4 . . . . . . . 
+            . . . . . . 4 5 5 4 . . . . . . 
+            . . . . . . 2 5 5 2 . . . . . . 
+            . . . . . . . 2 2 . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, mySprite, controller.dx(1000), controller.dy(1000))
     }
 })
 scene.onOverlapTile(SpriteKind.Projectile, sprites.dungeon.floorLight0, function (sprite, location) {
+	
+})
+scene.onHitWall(SpriteKind.Projectile, function (sprite, location) {
+    projectile.setImage(img`
+        . 3 . . . . . . . . . . . 4 . . 
+        . 3 3 . . . . . . . . . 4 4 . . 
+        . 3 d 3 . . 4 4 . . 4 4 d 4 . . 
+        . . 3 5 3 4 5 5 4 4 d d 4 4 . . 
+        . . 3 d 5 d 1 1 d 5 5 d 4 4 . . 
+        . . 4 5 5 1 1 1 1 5 1 1 5 4 . . 
+        . 4 5 5 5 5 1 1 5 1 1 1 d 4 4 . 
+        . 4 d 5 1 1 5 5 5 1 1 1 5 5 4 . 
+        . 4 4 5 1 1 5 5 5 5 5 d 5 5 4 . 
+        . . 4 3 d 5 5 5 d 5 5 d d d 4 . 
+        . 4 5 5 d 5 5 5 d d d 5 5 4 . . 
+        . 4 5 5 d 3 5 d d 3 d 5 5 4 . . 
+        . 4 4 d d 4 d d d 4 3 d d 4 . . 
+        . . 4 5 4 4 4 4 4 4 4 4 4 . . . 
+        . 4 5 4 . . 4 4 4 . . . 4 4 . . 
+        . 4 4 . . . . . . . . . . 4 4 . 
+        `)
+    projectile.setVelocity(0, 0)
     tiles.setTileAt(location, sprites.dungeon.darkGroundNorthWest0)
     tiles.setWallAt(location, false)
+    music.smallCrash.play()
+    projectile.destroy(effects.spray, 500)
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.floorLight3, function (sprite, location) {
     game.over(true)
